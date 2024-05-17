29650516e91d7b6ca1d75cd8dc2ef910a170b12f
Erez <erezmirav@gmail.com>
2024-05-17 12:11:43 +0300

    added onclick on time to toggle fullscreen

--- src/components/LiveClock.jsx
+++ src/components/LiveClock.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import Clock from 'react-live-clock';
-import '../App.css';
+import React from "react";
+import Clock from "react-live-clock";
+import "../App.css";
 
 const LiveClock = () => {
+  const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.error(
+          `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+        );
+      });
+    } else {
+      document.exitFullscreen().catch((err) => {
+        console.error(
+          `Error attempting to exit full-screen mode: ${err.message} (${err.name})`
+        );
+      });
+    }
+  };
+
   return (
     <div className="clock-container">
-      <h1 className="myClock">
+      <h1 className="myClock" onClick={toggleFullScreen}>
         <Clock interval={1000} ticking={true} />
       </h1>
       <h3 className="myDate">
-        <Clock format={'D.M.YY'} />
+        <Clock format={"D.M.YY"} />
       </h3>
     </div>
   );
